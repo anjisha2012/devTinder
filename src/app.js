e9b1c10026880b6e4c37ee0bcd4efe34970aae51
Anjali Singh <anjisha2012@gmail.com>
2024-10-05 20:33:11 +0530

    exploring get,post and delete calls

--- src/app.js
+++ src/app.js
@@ -2,10 +2,20 @@ const express = require('express');
 
 const app = express();
 
-app.use("/test" ,(req, res)=>{
-   res.send("hello from server");
+app.get("/user" , (req,res) => {
+    res.send({firstName: "Anjali" , lastName: "Singh"})
 })
 
+// app.use("/test" ,(req, res)=>{
+//    res.send("hello from server");
+// })
+
+app.post("/user" , (req,res) => {
+    res.send("data successfully send to the user");
+})
+app.delete("/user" , (req,res) => {
+    res.send("deleted");
+})
 app.listen(3000, ()=>{
     console.log("listening....")
 });
